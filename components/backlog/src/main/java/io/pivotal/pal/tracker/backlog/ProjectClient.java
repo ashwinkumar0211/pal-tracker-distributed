a101ba28f304341b6215a92d41557e16ba3712a8
ashwinkumar0211 <ashwinkumar02@yahoo.com>
2020-03-03 14:53:45 +0530

    Circuit Breaker

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -16,13 +16,19 @@ public class ProjectClient {
     private final String endpoint;
 
 
+
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
         this.restOperations = restOperations;
         this.endpoint = registrationServerEndpoint;
     }
     @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
-        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+
+        projectsCache.put(projectId, project);
+
+        return project;
+        //return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
     }
 
     public ProjectInfo getProjectFromCache(long projectId) {
